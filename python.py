0d2b423dfaf8db4f37c7b8d1fe67f6b6d9aa4197
Hug0Fer <hugolfernandes9@gmail.com>
2024-12-20 11:31:35 +0000

    LiguagemNatural python.py

---
+++ python.py
@@ -0,0 +1,22 @@
+'''
+    Programador....: (C) 2024 Hugo Fernandes
+    Data...........: 20/12/2024
+    Observações....: Operações Básicas em Python.
+'''
+
+n1 = 0
+n2 = 0
+soma = 0
+subtracao = 0
+divisao = 0
+
+n1 = int(input("Introduza um nº inteiro"))
+n2 = int(input("Introduza outro nº inteiro"))
+
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print("Soma:", soma)
